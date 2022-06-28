1db040627c53b317052348528238aa503e38a08a
nts-sixblack <nts.sixblack@gmail.com>
2022-06-28 13:30:06 +0700

    swagger

--- src/main/java/enclave/encare/encare/EnCareApplication.java
+++ src/main/java/enclave/encare/encare/EnCareApplication.java
@@ -1,9 +1,11 @@
 package enclave.encare.encare;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class EnCareApplication {
 
     public static void main(String[] args) {
