9ed533dd31b28bcb979292d8460fb9400da4c0a2
duckwin <tudaydada@gmail.com>
2022-07-13 14:26:28 +0700

    .

--- src/main/java/enclave/encare/encare/service/DoctorService.java
+++ src/main/java/enclave/encare/encare/service/DoctorService.java
@@ -12,14 +12,24 @@ import java.util.List;
 
 public interface DoctorService {
     DoctorResponse findById(long id);
-    DoctorResponse findByName(String name);
+
+    List<DoctorResponse> findByName(String name);
+
     String updateInfor(DoctorInformationForm doctorInformationForm) throws ParseException;
+
     boolean register(RegisterFormDoctor registerFormDoctor);
-    boolean changeStatusAppointment(long appointmentId,int statusId);
+
+    boolean changeStatusAppointment(long appointmentId, int statusId);
+
     List<DoctorResponse> listDoctor();
+
     List<DoctorResponse> listDoctor(int page);
+
     List<DoctorResponse> listDoctorOfHospital(long hospitalId);
-    List<DoctorResponse> listDoctorOfHospital(long hospitalId,int page);
+
+    List<DoctorResponse> listDoctorOfHospital(long hospitalId, int page);
+
     List<DoctorResponse> listDoctorOfCategory(long categoryId);
+
     List<DoctorResponse> listDoctorOfCategory(long categoryId, int page);
 }
