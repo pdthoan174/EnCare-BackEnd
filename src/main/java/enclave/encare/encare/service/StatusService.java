686c7ce76b11d0d112baf3593c2bfda31838562e
duckwin <tudaydada@gmail.com>
2022-07-20 11:47:12 +0700

    .

--- src/main/java/enclave/encare/encare/service/StatusService.java
+++ src/main/java/enclave/encare/encare/service/StatusService.java
@@ -1,7 +1,11 @@
 package enclave.encare.encare.service;
 
+import enclave.encare.encare.model.Status;
 import enclave.encare.encare.modelResponse.StatusResponse;
+
+import java.util.List;
 
 public interface StatusService {
     StatusResponse findById(long id);
+    List<StatusResponse> findAll();
 }
