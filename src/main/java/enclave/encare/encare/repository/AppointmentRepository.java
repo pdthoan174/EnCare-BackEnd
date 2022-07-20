1b0fd5f27fd7bde4d2971b1e96834947f1cff4e8
duckwin <tudaydada@gmail.com>
2022-07-20 11:42:39 +0700

    Search query

--- src/main/java/enclave/encare/encare/repository/AppointmentRepository.java
+++ src/main/java/enclave/encare/encare/repository/AppointmentRepository.java
@@ -18,6 +18,14 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
     List<Appointment> findByDoctorDoctorId(long doctorId);
     List<Appointment> findByDoctor_Hospital_HospitalId(long hospitalId);
     List<Appointment> findByStatusStatusId(long statusId);
-    @Query("SELECT ap from Appointment ap WHERE ap.user.account.phone like %?1% ")
-    List<Appointment> findByUser_Account_PhoneContains(String phone);
+    @Query("SELECT ap from Appointment ap WHERE ap.user.account.phone like %?1% and ap.doctor.account.accountId= ?2")
+    List<Appointment> find_by_phone_and_accountId(String phone,Long accountId);
+    @Query("SELECT ap from Appointment ap WHERE ap.user.account.name like %?1% and ap.doctor.account.accountId= ?2 ")
+    List<Appointment> find_by_name_and_accountId(String name,Long accountId);
+    @Query("SELECT ap from Appointment ap WHERE ap.description like %?1% and ap.doctor.account.accountId= ?2 ")
+    List<Appointment> find_by_descriptions_and_accountId(String descriptions,Long accountId);
+    @Query("SELECT ap from Appointment ap WHERE ap.symptoms like %?1% and ap.doctor.account.accountId= ?2")
+    List<Appointment> find_by_symptoms_and_accountId(String symptoms,Long accountId);
+    List<Appointment> findByStatusStatusIdAndAndDoctor_Account_AccountIdOrderByCreateDate(Long statusId,Long accountId);
+    Appointment findByAppointmentIdAndAndDoctor_Account_AccountIdOrderByCreateDate(Long appointmentId,Long accountId);
 }
