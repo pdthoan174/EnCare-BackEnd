e7784b1247280eefc91df6482b8f54ac220680fd
duckwin <tudaydada@gmail.com>
2022-07-20 11:47:42 +0700

    .

--- src/main/java/enclave/encare/encare/service/UserService.java
+++ src/main/java/enclave/encare/encare/service/UserService.java
@@ -4,7 +4,10 @@ import enclave.encare.encare.form.RegisterFormUser;
 import enclave.encare.encare.modelResponse.UserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public interface UserService {
     UserResponse findById(long id);
     boolean register(RegisterFormUser registerFormUser);
+    List<UserResponse> findAll();
 }
