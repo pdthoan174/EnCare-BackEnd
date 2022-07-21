b83a314fea41eef95cb2a0bc78e5cc5ca188e41a
nts-sixblack <nts.sixblack@gmail.com>
2022-07-22 00:10:27 +0700

    cors

--- src/main/java/enclave/encare/encare/config/WebMvcConfig.java
+++ src/main/java/enclave/encare/encare/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
                         "Access-Control-Request-Headers")
                 .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
